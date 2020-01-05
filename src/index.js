93501c915fdd834adbd8bea22b5a0d232ad16899
goepi <goepi@gmail.com>
2020-01-05 20:06:26 +0100

    Clean up

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ const http = require("http");
 const getTemplate = (templateName, callback) => {
   if (templateName.length > 0) {
     const templatesDir = path.join(__dirname, "/templates/");
-    console.log(templatesDir, __dirname, `${templatesDir}/${templateName}.js`);
-    fs.readFile(`${templatesDir}/${templateName}.html`, "utf8", (err, str) => {
+
+    const filePath = `${templatesDir}/${templateName}.html`;
+
+    console.log(templatesDir, __dirname, filePath);
+
+    fs.readFile(filePath, "utf8", (err, str) => {
       if (!err && str && str.length > 0) {
         callback(false, str);
       } else {
